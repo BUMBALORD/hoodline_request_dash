b3d620568a6a49ef2beedea1cb6fdbd1fa9a78f5
BUMBALORD <davidhallinan@gmail.com>
2016-10-20 18:55:46 -0700

    remove old code in controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,25 +1,6 @@
 class WelcomeController < ApplicationController
   def index
     @requests = Request.all
-
-    # file = File.open("#{Rails.root}/db/seed/resources.txt", 'r')
-    # content = file.read
-    # addresses = content.scan(/\d+\.\d+\.\d+\.\d+/)
-    # dates = content.scan(/\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/)
-    # matched = Hash[addresses.zip(dates)]
-
-    # matched.each do |address, date|
-    #   if Request.exists?(ip_address: address)
-    #     Request.where(ip_address: address).each do |request|
-    #       request[:dates] << date rescue nil
-    #     end
-    #   else
-    #     Request.create(
-    #       ip_address: address,
-    #       dates: date
-    #     )
-    #     end
-    #   end
   end
 
   private
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,3 @@
 class Request < ActiveRecord::Base
   serialize :dates
-
 end
